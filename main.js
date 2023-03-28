f8014959b7c9e8f601906b99a0f40e2164723dca
leep1000 <leepalmerformal@gmail.com>
2023-03-28 21:57:41 +0100

    adding to personal repo

--- main.js
+++ main.js
@@ -8,36 +8,124 @@ The user will click on one of the 3 buttons to play the game. The computer will 
 The user will be able to see the score of the game and the amount of rounds they have played after each round. The score will be displayed in the html. 
 The user will receive a special message if they win 3 rounds in a row. The user will receive a special message if they lose 3 rounds in a row. The user will receive a special message if they draw 3 rounds in a row.
 */
-// Get elements from HTML
-const usernameInput = document.getElementById("username");
-const displayUsername = document.getElementById("display-username");
-const roundsPlayed = document.getElementById("rounds-played");
-const wins = document.getElementById("wins");
-const losses = document.getElementById("losses");
-const draws = document.getElementById("draws");
-const scoreMessage = document.getElementById("score-message");
-
-// Initialize game variables
-let userScore = 0;
-let computerScore = 0;
-let drawScore = 0;
-let userChoice;
-let computerChoice;
-let round = 1;
+// Define global variables
+// defining the variablesx and their values at that start of the game.
+// Also defines winning combinations of variable
+let score = 0;
+let roundsPlayed = 0;
 let consecutiveWins = 0;
 let consecutiveLosses = 0;
 let consecutiveDraws = 0;
+const winningCombos = {
+	'rock': 'scissors',
+	'paper': 'rock',
+	'scissors': 'paper'
+};
 
-// Function to play the game
-function playGame(userChoice) {
-  // Get computer choice
-  const choices = ["rock", "paper", "scissors"];
-  computerChoice = choices[Math.floor(Math.random() * choices.length)];
+// Play function when user clicks on one of the buttons
+function play(userChoice) {
+	// Generate computer choice randomly
+	const choices = ['rock', 'paper', 'scissors'];
+	const computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
-  // Determine winner
-  if (userChoice === computerChoice) {
-    drawScore++;
-    consecutiveDraws++;
-    scoreMessage.textContent = "It's a draw!";
-  } else if (
-    (userChoice === "rock" && computerChoice === "scissors
+	// Determine winner and update score
+	if (userChoice === computerChoice) {
+		consecutiveDraws++;
+		showSpecialMessage('draw');
+	} else if (winningCombos[userChoice] === computerChoice) {
+		score++;
+		consecutiveWins++;
+		showSpecialMessage('win');
+	} else {
+		score--;
+		consecutiveLosses++;
+		showSpecialMessage('lose');
+	}
+
+	// Update rounds played counter and display score
+	roundsPlayed++;
+	document.getElementById('score').innerText = score;
+	document.getElementById('rounds-played').innerText = roundsPlayed;
+	checkConsecutiveGames();
+}
+
+// Function to check for consecutive wins, losses, or draws
+function checkConsecutiveGames() {
+	if (consecutiveWins === 3) {
+		showSpecialMessage('consecutiveWins');
+	} else if (consecutiveLosses === 3) {
+		showSpecialMessage('consecutiveLosses');
+	} else if (consecutiveDraws === 3) {
+		showSpecialMessage('consecutiveDraws');
+	} else {
+		return;
+	}
+}
+
+// Function to show special messages for consecutive wins, losses, or draws
+function showSpecialMessage(messageType) {
+	const messageElement = document.getElementById('special-message');
+	switch (messageType) {
+		case 'win':
+			messageElement.innerText = 'You won!';
+			break;
+		case 'lose':
+			messageElement.innerText = 'You lost!';
+			break;
+		case 'draw':
+			messageElement.innerText = 'It\'s a tie!';
+			break;
+		case 'consecutiveWins':
+			messageElement.innerText = 'Wow, you\'ve won 3 times in a row!';
+			break;
+		case 'consecutiveLosses':
+			messageElement.innerText = 'Oops, you\'ve lost 3 times in a row.';
+			break;
+		case 'consecutiveDraws':
+			messageElement.innerText = 'You\'ve drawn 3 times in a row.';
+			break;
+		default:
+			messageElement.innerText = '';
+			break;
+	}
+}
+
+// Function to reset the score. Every variable goes back to their original value once this function is called
+function resetScore() {
+	score = 0;
+	roundsPlayed = 0;
+	consecutiveWins = 0;
+	consecutiveLosses = 0;
+	consecutiveDraws = 0;
+	document.getElementById('score').innerText = score;
+	document.getElementById('rounds-played').innerText = roundsPlayed;
+	document.getElementById('special-message').innerText = '';
+}
+
+// Function to play the game again
+function playAgain() {
+	document.getElementById('special-message').innerText = '';
+    consecutiveWins = 0;
+    consecutiveLosses = 0;
+    consecutiveDraws = 0;
+
+}
+
+// Function to end the game
+function endGame() {
+	alert('Final score: ' + score + '\nRounds played: ' + roundsPlayed);
+	resetScore();
+}
+
+const usernameInput = document.getElementById("username");
+const confirmButton = document.getElementById("confirm-username");
+const greeting = document.getElementById("greeting");
+
+confirmButton.addEventListener("click", () => {
+    if (usernameInput.value) {
+      greeting.textContent = `Let's play, ${usernameInput.value}!`;
+    } else {
+      greeting.textContent = "Ready?";
+    }
+  });
+
